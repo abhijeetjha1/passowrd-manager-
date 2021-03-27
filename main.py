fa9399952cdf81caf96140a212b4fdebccd3275e
Abhijeet <discoverwithabhi@gmail.com>
2021-03-26 23:39:36 -0700

    password genrator added

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def genrate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+      password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+      password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+      password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+      password += char
 
 
+    password_input.insert(END,password)
 
 
 
@@ -32,7 +59,7 @@ password_text.grid(row=3,column=0)
 password_input=Entry(width=27)
 password_input.grid(row=3,column=1)
 
-gen_butt=Button(text="Generate Password",highlightthickness=0)
+gen_butt=Button(text="Generate Password",highlightthickness=0,command=genrate_password)
 gen_butt.grid(row=3,column=2)
 
 
